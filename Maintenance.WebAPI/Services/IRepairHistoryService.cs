a17b4a7aa78b1438563245493bd63af98b3f4d43
DizzaHarkhani1106 <dharkhani2375@conestogac.on.ca>
2026-02-14 00:37:23 -0500

    added backend api project

---
+++ Maintenance.WebAPI/Services/IRepairHistoryService.cs
@@ -0,0 +1,10 @@
+﻿using Maintenance.WebAPI.Models;
+
+namespace Maintenance.WebAPI.Services
+{
+    public interface IRepairHistoryService
+    {
+        List<RepairHistoryDto> GetByVehicleId(int vehicleId);
+    }
+}
+    
